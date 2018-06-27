04b1aacc9fbda84b3f96b1dfb88230a5709fcf64
Jess Terry <jterry@spillman.com>
2018-06-27 14:41:47 -0600

    Repo Init - Base Project

---
+++ RectangleIntersection/RectangleIntersection/Interfaces/IDetectIntersection.cs
@@ -0,0 +1,19 @@
+﻿using Geometry.Shapes;
+
+namespace Geometry.Interfaces
+{
+	/// <summary>
+	/// Contract for all classes which are able to detect intersection between geometric shapes
+	/// </summary>
+	public interface IDetectIntersection
+	{
+		/// <summary>
+		/// Determines whether two rectangles intersect
+		/// </summary>
+		/// <param name="rectangle1">The first rectangle</param>
+		/// <param name="rectangle2">The second rectangle</param>
+		/// <returns>True if the rectangles intersect, else, false.</returns>
+		/// <exception cref="ArgumentException">Thrown if either rectangle is null</exception>
+		bool DoesIntersect(Rectangle rectangle1, Rectangle rectangle2);
+	}
+}
---
+++ RectangleIntersection/RectangleIntersection/Shapes/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Geometry.Shapes
+{
+	/// <summary>
+	/// A rectangular shape
+	/// </summary>
+	public class Rectangle
+	{
+		#region Ctor
+
+		/// <summary>
+		/// Create a Rectangle
+		/// </summary>
+		/// <param name="x1">The first X-coord</param>
+		/// <param name="y1">The first Y-coord</param>
+		/// <param name="x2">The second X-coord</param>
+		/// <param name="y2">The second Y-coord</param>
+		public Rectangle(int x1, int y1, int x2, int y2)
+		{
+			X1 = x1;
+			X2 = x2;
+			Y1 = y1;
+			Y2 = y2;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Get / set the first X-coord
+		/// </summary>
+		public int X1 { get; set; }
+		/// <summary>
+		/// Get / set the first Y-coord
+		/// </summary>
+		public int Y1 { get; set; }
+		/// <summary>
+		/// Get / set the second X-coord
+		/// </summary>
+		public int X2 { get; set; }
+		/// <summary>
+		/// Get / set the second Y-coord
+		/// </summary>
+		public int Y2 { get; set; }
+
+		#endregion
+	}
+}
